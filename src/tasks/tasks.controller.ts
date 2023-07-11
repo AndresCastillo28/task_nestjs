c6ac5ac4ef9569dec39d0922f57ee2d278c77411
AndresCastillo28 <ansa2000@hotmail.es>
2023-07-11 11:07:02 -0500

    First commit

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Delete, Get, Param, Post, Put, Patch } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto, UpdateTaskDto } from './dto/task.dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private taskService: TasksService) {}
+
+  @Get()
+  getAllTasks() {
+    return this.taskService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body() newTask: CreateTaskDto) {
+    return this.taskService.createTask(newTask.title, newTask.description);
+  }
+
+  @Delete(':id')
+  deleteTask(@Param('id') id: string) {
+    this.taskService.deleteTask(id)
+  }
+
+  @Patch(":id")
+  updateTask(@Param("id") id: string, @Body() updatedFileds: UpdateTaskDto) {
+    return this.taskService.updateTask(id, updatedFileds); 
+  }
+}
